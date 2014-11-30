8f02fe9d814c7e744f66fdffef697b3b43b3af7f
Eli Yukelzon <reflog@gmail.com>
2014-11-30 13:19:40 +0200

    progress

--- src/CDVreflogcamera.m
+++ src/CDVreflogcamera.m
@@ -2,11 +2,12 @@
 
 #import "Cordova/CDV.h"
 #import "BDCameraViewController.h"
+#import "MBProgressHUD.h"
 
 @interface CDVreflogcamera : CDVPlugin <UINavigationControllerDelegate, CameraDelegate>{}
 
 @property (copy) NSString* callbackId;
-
+@property (strong, nonatomic) BDCameraViewController *cameraContainer;
 - (void)openCamera:(CDVInvokedUrlCommand*)command;
 @end
 
@@ -14,44 +15,75 @@
 
 - (void)openCamera:(CDVInvokedUrlCommand*)command
 {
-    id title = [command.arguments objectAtIndex:0];
-    self.callbackId = command.callbackId;
-    BDCameraViewController *cameraContainer = [[BDCameraViewController alloc] initWithDelegate:self];
-    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:cameraContainer];
+    self.cameraContainer = [[BDCameraViewController alloc] initWithDelegate:self];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:self.cameraContainer];
     nav.delegate = self;
-    cameraContainer.title = title;
     [nav setNavigationBarHidden:NO];
     [self.viewController presentViewController:nav animated:YES completion:nil];
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [pluginResult setKeepCallbackAsBool:YES];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+- (void)takePhoto:(CDVInvokedUrlCommand*)command
+{
+    self.callbackId = command.callbackId;
+    id title = [command.arguments objectAtIndex:0];
+    self.cameraContainer.title = title;
+}
+
+
 #define _PHOTO_PREFIX @"RFCAMERA_"
 - (void) imageCaptured:(UIImage *)image
 {
-    NSData* data = UIImagePNGRepresentation(image);
-    NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
-    NSError* err = nil;
-    NSFileManager* fileMgr = [[NSFileManager alloc] init];
-    NSString* filePath;
+    
+    MBProgressHUD* hud = [MBProgressHUD showHUDAddedTo:self.cameraContainer.view animated:YES];
+    hud.labelText = @"Saving...";
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        NSData* data = UIImagePNGRepresentation(image);
+        
+        NSArray *dirPaths;
+        dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                       NSUserDomainMask, YES);
+        NSString* docsPath = [dirPaths objectAtIndex:0];
+        
+        NSError* err = nil;
+        NSFileManager* fileMgr = [[NSFileManager alloc] init];
+        NSString* filePath;
+        NSString* fileName;
+        
+        int i = 1;
+        do {
+            fileName = [NSString stringWithFormat:@"%@%03d.%@",_PHOTO_PREFIX, i++, @"png"];
+            filePath = [NSString stringWithFormat:@"%@/%@", docsPath, fileName];
+        } while ([fileMgr fileExistsAtPath:filePath]);
+        
+        __block CDVPluginResult* pluginResult = nil;
+        
+        if (![data writeToFile:filePath options:NSAtomicWrite error:&err]) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:[err localizedDescription]];
+        } else {
+            NSMutableDictionary* resultDictionary = [[NSMutableDictionary alloc] init];
+            [resultDictionary setValue:[[NSURL fileURLWithPath:fileName] absoluteString] forKey:@"imageURL"];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:resultDictionary];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [MBProgressHUD hideHUDForView:self.cameraContainer.view animated:YES];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
+        });
+    });
+    
+   }
 
-    int i = 1;
-    do {
-        filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, _PHOTO_PREFIX, i++, @"png"];
-    } while ([fileMgr fileExistsAtPath:filePath]);
-
-    CDVPluginResult* pluginResult = nil;
-
-    if (![data writeToFile:filePath options:NSAtomicWrite error:&err]) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:[err localizedDescription]];
-    } else {
-        NSMutableDictionary* resultDictionary = [[NSMutableDictionary alloc] init];
-        [resultDictionary setValue:[[NSURL fileURLWithPath:filePath] absoluteString] forKey:@"imageURL"];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:resultDictionary];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
-}
-
-- (void)closeCamera:(CDVInvokedUrlCommand*)cmd {
-    [self.viewController dismissViewControllerAnimated:YES completion:nil];
+- (void)closeCamera:(CDVInvokedUrlCommand*)command {
+    [self.viewController dismissViewControllerAnimated:YES completion:^{
+        self.cameraContainer = nil;
+        NSLog(@"sending result to %@",command.callbackId);
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
 #pragma mark - UINavigationControllerDelegate
--- www/camera.js
+++ www/camera.js
@@ -1,17 +1,25 @@
-var exec = require('cordova/exec');
+cordova.define("org.reflog.camera.plugin.ReflogCamera", function(require, exports, module) { var exec = require('cordova/exec');
 
 _logMessage = function(message){
   return console.log(message);
 };
 
-exports.openCamera = function(title, success, error) {
-    success = success || _logMessage;
-    error = error || _logMessage;
-    exec(success, error, "reflogcamera", "openCamera", [title]);
+exports.openCamera = function(success, error) {
+  success = success || _logMessage;
+  error = error || _logMessage;
+  exec(success, error, "reflogcamera", "openCamera", []);
+};
+
+exports.takePhoto = function(success, error, title) {
+  success = success || _logMessage;
+  error = error || _logMessage;
+  exec(success, error, "reflogcamera", "takePhoto", [title]);
 };
 
 exports.closeCamera = function(success, error) {
-    success = success || _logMessage;
-    error = error || _logMessage;
-    exec(success, error, "reflogcamera", "closeCamera", []);
+  success = success || _logMessage;
+  error = error || _logMessage;
+  exec(success, error, "reflogcamera", "closeCamera", []);
 };
+
+});
